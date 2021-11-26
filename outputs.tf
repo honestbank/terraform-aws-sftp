69e426916987f6427d4cdc6afb7c241365308d4d
Phathaisarn Chalermchuang <phathaisarn@honestbank.com>
2021-11-26 10:46:30 +0700

    fix: add target S3 bucket name and ARN output

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,19 @@ output "transfer_server_id" {
 output "transfer_server_endpoint" {
   value = module.sftp.transfer_server_endpoint
 }
+
+output "transfer_storage_bucket_name" {
+  value = module.sftp.transfer_storage_bucket_name
+}
+
+output "transfer_storage_bucket_arn" {
+  value = module.sftp.transfer_storage_bucket_arn
+}
+
+output "target_storage_bucket_name" {
+  value = module.sftp.target_storage_bucket_name
+}
+
+output "target_storage_bucket_arn" {
+  value = module.sftp.target_storage_bucket_arn
+}
